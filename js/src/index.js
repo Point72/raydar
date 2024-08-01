a31045f0527da214e3ce1e0c1c447e6c0be34d84
Todd Gaugler <todd.gaugler@cubistsystematic.com>
2024-08-01 15:31:43 -0400

    Removing trailing slashes where applicable
    Signed-off-by: Todd Gaugler <todd.gaugler@cubistsystematic.com>

--- js/src/index.js
+++ js/src/index.js
@@ -7,17 +7,25 @@ import "@finos/perspective-viewer/dist/css/themes.css";
 import "@finos/perspective-workspace";
 import "./index.css";
 
+function removeTrailingSlash(url) {
+  return url.replace(/\/$/, "");
+}
+
 window.addEventListener("load", async () => {
   const workspace = document.querySelector("perspective-workspace");
   const protocol = window.location.protocol === "https:" ? "wss:" : "ws:";
   const websocket = perspective.websocket(
-    `${protocol}//${window.location.host}${window.location.pathname}/ws`,
+    `${protocol}//${window.location.host}${removeTrailingSlash(
+      window.location.pathname,
+    )}/ws`,
   );
   const registeredTables = new Set();
   const worker = perspective.worker();
 
   const updateTables = async () => {
-    const response = await fetch(`${window.location.href}/tables`);
+    const response = await fetch(
+      `${removeTrailingSlash(window.location.href)}/tables`,
+    );
     const tables = await response.json();
 
     tables.map(async (tableName) => {
