01cc544b76aa86d12e7e43427972f366cd836475
Todd Gaugler <todd.gaugler@cubistsystematic.com>
2024-09-23 08:57:45 -0400

    Adding title bar + save/load layout functionality
    Signed-off-by: Todd Gaugler <todd.gaugler@cubistsystematic.com>
    
    ...

--- js/src/index.js
+++ js/src/index.js
@@ -13,6 +13,7 @@ function removeTrailingSlash(url) {
 
 window.addEventListener("load", async () => {
   const workspace = document.querySelector("perspective-workspace");
+  const saveButton = document.getElementById("save-layout-button");
   const protocol = window.location.protocol === "https:" ? "wss:" : "ws:";
   const websocket = perspective.websocket(
     `${protocol}//${window.location.host}${removeTrailingSlash(
@@ -34,6 +35,51 @@ window.addEventListener("load", async () => {
       workspace.addTable(tableName, websocket.open_table(tableName));
     });
   };
+
+  document.body.addEventListener("dragover", (e) => {
+    e.preventDefault();
+  });
+
+  document.body.addEventListener("drop", (e) => {
+    e.preventDefault();
+
+    const file = e.dataTransfer.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (event) => {
+        try {
+          const json = JSON.parse(event.target.result);
+          workspace.restore(json);
+          console.log("File contents:", json);
+        } catch (error) {
+          console.error("Error parsing JSON:", error);
+        }
+      };
+      reader.readAsText(file);
+    }
+  });
+
+  saveButton.addEventListener("click", function () {
+    let workspace = document.querySelector("perspective-workspace");
+
+    workspace.save().then((config) => {
+      // Convert the configuration object to a JSON string
+      let json = JSON.stringify(config);
+
+      // Create a Blob object from the JSON string
+      let blob = new Blob([json], { type: "application/json" });
+
+      // Create a download link
+      let link = document.createElement("a");
+      link.href = URL.createObjectURL(blob);
+      link.download = "workspace.json";
+
+      // Append the link to the document body and click it to start the download
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    });
+  });
 
   const layouts = await fetch("static/layouts/default.json");
   const layoutData = await layouts.json();
