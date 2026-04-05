322bded2c4e25f5d1a622f392a1db2081c36eeff
Tim Paine <3105306+timkpaine@users.noreply.github.com>
2026-04-05 19:46:24 -0400

    fix conflicts
    Signed-off-by: Tim Paine <3105306+timkpaine@users.noreply.github.com>

--- js/build.mjs
+++ js/build.mjs
@@ -1,22 +1,7 @@
-import { build } from "@finos/perspective-esbuild-plugin/build.js";
-<<<<<<< before updating
-import { BuildCss } from "@prospective.co/procss/target/cjs/procss.js";
-import cpy from "cpy";
-import fs from "fs";
-import { createRequire } from "node:module";
-=======
-import { transform } from "lightningcss";
-import { getarg } from "./tools/getarg.mjs";
+import { build } from "./tools/build.js";
+import { compile } from "./tools/css.js";
 import fs from "fs";
 import cpy from "cpy";
-
-const DEBUG = getarg("--debug");
-
-const COMMON_DEFINE = {
-  global: "window",
-  "process.env.DEBUG": `${DEBUG}`,
-};
->>>>>>> after updating
 
 const BUILD = [
   {
@@ -40,94 +25,6 @@ const BUILD = [
   },
 ];
 
-<<<<<<< before updating
-const require = createRequire(import.meta.url);
-function add(builder, path, path2) {
-  builder.add(path, fs.readFileSync(require.resolve(path2 || path)).toString());
-=======
-async function compile_css() {
-  const process_path = (path) => {
-    const outpath = path.replace("src/css", "dist/css");
-    fs.mkdirSync(outpath, { recursive: true });
-
-    fs.readdirSync(path, { withFileTypes: true }).forEach((entry) => {
-      const input = `${path}/${entry.name}`;
-      const output = `${outpath}/${entry.name}`;
-
-      if (entry.isDirectory()) {
-        process_path(input);
-      } else if (entry.isFile() && entry.name.endsWith(".css")) {
-        const source = fs.readFileSync(input);
-        const { code } = transform({
-          filename: entry.name,
-          code: source,
-          minify: !DEBUG,
-          sourceMap: false,
-        });
-        fs.writeFileSync(output, code);
-      }
-    });
-  };
-
-  process_path("src/css");
->>>>>>> after updating
-}
-
-async function compile_css() {
-  const builder1 = new BuildCss("");
-  add(builder1, "./src/index.css");
-  add(
-    builder1,
-    "perspective-viewer-pro.css",
-    "@perspective-dev/viewer/dist/css/pro.css",
-  );
-  add(
-    builder1,
-    "perspective-viewer-pro-dark.css",
-    "@perspective-dev/viewer/dist/css/pro-dark.css",
-  );
-  add(
-    builder1,
-    "perspective-viewer-monokai.css",
-    "@perspective-dev/viewer/dist/css/monokai.css",
-  );
-  add(
-    builder1,
-    "perspective-viewer-vaporwave.css",
-    "@perspective-dev/viewer/dist/css/vaporwave.css",
-  );
-  add(
-    builder1,
-    "perspective-viewer-dracula.css",
-    "@perspective-dev/viewer/dist/css/dracula.css",
-  );
-  add(
-    builder1,
-    "perspective-viewer-gruvbox.css",
-    "@perspective-dev/viewer/dist/css/gruvbox.css",
-  );
-  add(
-    builder1,
-    "perspective-viewer-gruvbox-dark.css",
-    "@perspective-dev/viewer/dist/css/gruvbox-dark.css",
-  );
-  add(
-    builder1,
-    "perspective-viewer-solarized.css",
-    "@perspective-dev/viewer/dist/css/solarized.css",
-  );
-  add(
-    builder1,
-    "perspective-viewer-solarized-dark.css",
-    "@perspective-dev/viewer/dist/css/solarized-dark.css",
-  );
-
-  const css = builder1.compile().get("index.css");
-
-  // write to extension
-  fs.writeFileSync("./dist/index.css", css);
-}
-
 async function build_all() {
   /* make directories */
   fs.mkdirSync("../raydar/dashboard/static/", { recursive: true });
@@ -149,7 +46,7 @@ async function build_all() {
   );
 
   /* Compile css */
-  await compile_css();
+  await compile();
 
   /* Copy to raydar static */
   await cpy("./dist/*", "../raydar/dashboard/static/", {
